0fe7b8dedd3b5e0a4770b20b559a0cb58044c5ee
martin-martin <breuss.martin@gmail.com>
2018-09-25 14:21:01 +0200

    (4) complete forms 🔓

--- djitter/djeeterprofile/forms.py
+++ djitter/djeeterprofile/forms.py
@@ -6,24 +6,24 @@ from django.utils.html import strip_tags
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(required=True,
-                             widget=forms.widget.TextInput(
+                             widget=forms.widgets.TextInput(
                                     attrs={'placeholder': 'Email',
                                            'class': 'form-control'}))
     first_name = forms.CharField(required=True,
-                                 widget=forms.widget.TextInput(
+                                 widget=forms.widgets.TextInput(
                                     attrs={'placeholder': 'First Name',
                                            'class': 'form-control'}))
     last_name = forms.CharField(required=True,
-                                widget=forms.widget.TextInput(
+                                widget=forms.widgets.TextInput(
                                     attrs={'placeholder': 'Last Name',
                                            'class': 'form-control'}))
-    username = forms.CharField(widget=forms.widget.TextInput(
+    username = forms.CharField(widget=forms.widgets.TextInput(
                                     attrs={'placeholder': 'Username',
                                            'class': 'form-control'}))
-    password1 = forms.CharField(widget=forms.widget.PasswordInput(
+    password1 = forms.CharField(widget=forms.widgets.PasswordInput(
                                     attrs={'placeholder': 'Password',
                                            'class': 'form-control'}))
-    password2 = forms.CharField(widget=forms.widget.PasswordInput(
+    password2 = forms.CharField(widget=forms.widgets.PasswordInput(
                                     attrs={'placeholder': 'Password Confirmation',
                                            'class': 'form-control'}))
 
@@ -38,9 +38,9 @@ class SignupForm(UserCreationForm):
 
 
 class SigninForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.widget.TextInput(
+    username = forms.CharField(widget=forms.widgets.TextInput(
                                     attrs={'placeholder': 'Username',
                                            'class': 'form-control'}))
-    password = forms.CharField(widget=forms.widget.TextInput(
+    password = forms.CharField(widget=forms.widgets.TextInput(
                                     attrs={'placeholder': 'Password',
                                            'class': 'form-control'}))
