6a6ec35269769c46a4c3882b6a2322a75ab5475c
martin-martin <breuss.martin@gmail.com>
2018-09-28 16:57:50 +0200

    fix Feed page

--- djitter/djeet/views.py
+++ djitter/djeet/views.py
@@ -5,13 +5,18 @@ from .models import Djeet
 
 @login_required
 def feed(request):
+    print("hei there!")
     userids = []
 
-    for id in request.user.djeeterprofile.follows.all():
-        userids.append(id)
+    # gives us the whole user profile
+    for user in request.user.djeeterprofile.follows.all():
+        # collecting the IDs of user's the user is following
+        userids.append(user.id)
 
-    userids.append(request.user.id)  # add own djeets
-    # get all objects from the db and render 25 for display
+    userids.append(request.user.id)  # add own user ID
+
+    print(userids)
+    # get all corresponding objects from the db and render 25 for display
     djeets = Djeet.objects.filter(user_id__in=userids[:25])
 
     return render(request, 'feed.html', {'djeets': djeets})
