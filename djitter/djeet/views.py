27f0f1543fefb25be093744bdc2d1a8377410019
martin-martin <breuss.martin@gmail.com>
2018-09-28 17:00:56 +0200

    fix: remove debug statements

--- djitter/djeet/views.py
+++ djitter/djeet/views.py
@@ -5,17 +5,12 @@ from .models import Djeet
 
 @login_required
 def feed(request):
-    print("hei there!")
     userids = []
-
     # gives us the whole user profile
     for user in request.user.djeeterprofile.follows.all():
         # collecting the IDs of user's the user is following
         userids.append(user.id)
-
     userids.append(request.user.id)  # add own user ID
-
-    print(userids)
     # get all corresponding objects from the db and render 25 for display
     djeets = Djeet.objects.filter(user_id__in=userids[:25])
 
