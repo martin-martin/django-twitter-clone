0092b36e092813bc61105282a1f9056825e6bc3d
martin-martin <breuss.martin@gmail.com>
2018-09-28 17:13:05 +0200

    remove own djeets from feed

--- djitter/djeet/views.py
+++ djitter/djeet/views.py
@@ -10,7 +10,8 @@ def feed(request):
     for user in request.user.djeeterprofile.follows.all():
         # collecting the IDs of user's the user is following
         userids.append(user.id)
-    userids.append(request.user.id)  # add own user ID
+    # NOTE: (optional) uncomment the below line to also add own tweets:
+    # userids.append(request.user.id)  # add own user ID
     # get all corresponding objects from the db and render 25 for display
     djeets = Djeet.objects.filter(user_id__in=userids[:25])
 
